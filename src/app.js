1da95c0eb57c7d02e594f473ec952b7a14a6c86e
Elías Milano <87205729+MilanoTester@users.noreply.github.com>
2024-07-17 19:30:24 +0000

    Project finished

--- src/app.js
+++ src/app.js
@@ -2,10 +2,37 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = [
+    "A wild horse",
+    "A massive dog",
+    "An angry old man",
+    "A cow over a truck"
+  ];
+  let action = ["bit", "pooped over", "crushed", "spit"];
+  let what = ["my portfolio", "my phone", "my car", "my suit"];
+  let when = [
+    "this morning",
+    "a few moments ago",
+    "going there",
+    "during my lunch"
+  ];
+
+  //to determine random indexes for arrays
+  let max = 3 + 1;
+  let min = 0;
+  let whoIndex = Math.floor(min + Math.random() * (max - min));
+  let actionIndex = Math.floor(min + Math.random() * (max - min));
+  let whatIndex = Math.floor(min + Math.random() * (max - min));
+  let whenIndex = Math.floor(min + Math.random() * (max - min));
+
+  document.getElementById("excuse").innerHTML =
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex];
 };
